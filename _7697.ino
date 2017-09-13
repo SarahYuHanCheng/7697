6a245041073f7cf7902dca308f5ca0d3c9e3815f
SarahCheng <bigbigtreeful@gmail.com>
2017-09-14 07:35:08 +0800

    lab1-1 Breathing USR Led
    USR Led on P7

--- _7697.ino
+++ _7697.ino
@@ -1,49 +1,20 @@
-#include <WiFiClient.h>
-#include <LWiFi.h>
-
-#define SSID "Biigtree"
-#define PASSWD "screamlab"
-#define TCP_IP "172.20.10.2"
-#define TCP_PORT 8888
-
-int status = WL_IDLE_STATUS;
-static unsigned long currentTimer = millis();
-
-WiFiClient wifiClient;
+# define ledpin 7
 
 void setup() {
-    Serial.begin(9600);
-    while (!Serial)
-      ;
-    while (status != WL_CONNECTED) { // 3
-        delay(500);
-        status =  WiFi.begin(SSID, PASSWD);
-        Serial.print("Attempting to connect to SSID: ");
-        Serial.println(SSID);
-        Serial.println(status);
-    }
-    // Conenct to AP successfully
-    while (!wifiClient.connect(TCP_IP, 8888)){
-      delay(300);
-      Serial.print("Attempting to connect to SERVER: ");
-        Serial.println(TCP_IP);
-    }
-
-   
+  
+ Serial.begin(9600);
+ pinMode(ledpin,OUTPUT);
 }
 
 void loop() {
   
-  wifiClient.println("good connection");
-  delay(2000);
-  currentTimer = millis();
-  if(millis() - currentTimer >= 10000) {
-    if(!wifiClient.connected()){
-      //reconnect
-      setup();
-    } else {
-      currentTimer = millis();
-    }
+ int i=0;
+ for(i;i<255;i++){
+  analogWrite(ledpin,i);
+  delay(10);
   }
-
+  for(i;i>0;i--){
+  analogWrite(ledpin,i);
+  delay(10);
+  }
 }
