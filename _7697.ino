8af4efd11a98175dc6168cbab590c7882d68159b
SarahCheng <bigbigtreeful@gmail.com>
2017-09-14 11:23:29 +0800

    lab1-3 AD switch between breathing and marquee

--- _7697.ino
+++ _7697.ino
@@ -1,32 +1,76 @@
-# define ledpin 10
-# define sw_btn 12
+# define ledpin1 10
+# define ledpin2 11
+# define ledpin3 12
+# define sw_btn 13
 
+
+int _status;
 void setup() {
   
  Serial.begin(9600);
- pinMode(ledpin,OUTPUT);
+ pinMode(ledpin1,OUTPUT);
+ pinMode(ledpin2,OUTPUT);
+ pinMode(ledpin3,OUTPUT);
  pinMode(sw_btn,INPUT);
 }
 
-int _status;
 void loop() {
 
- int btn_value1 = digitalRead(sw_btn);
- delay(200);
- int btn_value2 = digitalRead(sw_btn);
+ int btn_value1, btn_value2 ;
  Serial.println(btn_value1);
  Serial.println(btn_value2);
  Serial.println(_status);
  Serial.println("-------------------");
- 
+ btn_value1 = digitalRead(sw_btn);
+ delay(200);  
+ btn_value2 = digitalRead(sw_btn);
+    
  if(btn_value1!=btn_value2){
-  if(!_status){
-    _status = 1;
-  
- }else{
-  _status = 0;
-  }
-  }
-  digitalWrite(ledpin,_status);
-  delay(500);
+   _status ++;
+   switch(_status){
+   case 1:
+      {
+        int i=0;
+        for(i;i<255;i++){
+          analogWrite(ledpin1,i);
+          analogWrite(ledpin2,i);
+          analogWrite(ledpin3,i);
+          delay(10);
+        }
+        for(i;i>0;i--){
+        analogWrite(ledpin1,i);
+        analogWrite(ledpin2,i);
+        analogWrite(ledpin3,i);
+        delay(10);
+        }
+         
+        break;
+    }
+        
+    case 2:
+    {
+      for(int j=0;j<3;j++){
+        analogWrite(ledpin1,255);
+        analogWrite(ledpin3,0);
+      delay(500);
+        analogWrite(ledpin2,255);
+        analogWrite(ledpin1,0);
+      delay(500);
+        analogWrite(ledpin3,255);
+        analogWrite(ledpin2,0);
+      delay(500);
+      }
+      break;
+    }
+       
+   default:
+   {_status=0;
+   analogWrite(ledpin1,0);
+   analogWrite(ledpin2,0);
+   analogWrite(ledpin3,0);
+   }
+    break;
+    }
+ }
+    delay(300);
 }
