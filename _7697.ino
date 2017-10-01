bbf4eac6c4af3b9971134594efd0fc62000548a2
SarahCheng <bigbigtreeful@gmail.com>
2017-10-01 02:57:35 -0500

    W3_motor_basic

--- _7697.ino
+++ _7697.ino
@@ -1,113 +1,20 @@
-/* Receive the controlling message, turning on/off and pwm, and
- * than set the corresponding pin.
- */
-#include <WiFiClient.h>
-#include <LWiFi.h>
-#include <task.h>
+# define ledpin1 9
+# define ledpin2 10
 
-#define SSID "CSIE-WLAN"
-#define PASSWD "wificsie"
-#define TCP_IP "192.168.208.204"
-#define TCP_PORT 5000
-
-WiFiClient wifiClient;
-
-static char buf[32];
-static int messageLen;
-static char client_ID[] = "Man";
-static char *recv_ID,*recv_buf;
-
-
-
-IPAddress ip;
-void setup()
-{
-    int status = WL_IDLE_STATUS;
-    Serial.begin(115200);
-    while (!Serial)
-      ;       
-
-    
-
-    // set WiFi
-   // WiFi.mode(WIFI_STA);
-    WiFi.begin(SSID, PASSWD);
-    while (status != WL_CONNECTED) {
-        // Connect failed, blink 0.5 second to indicate
-        // the board is retrying.
-        delay(500);
-        WiFi.begin(SSID, PASSWD);
-        status =  WiFi.begin(SSID, PASSWD);
-        Serial.print("Attempting to connect to SSID: ");
-        Serial.println(SSID);
-        Serial.println(status);
-    }
-    
-    // Conenct to AP successfully
-   // wifiClient.connect(TCP_IP, TCP_PORT);
-      while (!wifiClient.connect(TCP_IP, TCP_PORT)){
-      delay(300);
-      Serial.print("Attempting to connect to SERVER: ");
-        Serial.println(TCP_IP);
-    }
-
-    reg_ID();
-
-    delay(1000);
-    xTaskCreate(
-                    askPos,          /* Task function. */
-                    "askPos",        /* String with name of task. */
-                    10000,            /* Stack size in words. */
-                    NULL,             /* Parameter passed as input of the task */
-                    1,                /* Priority of the task. */
-                    NULL);            /* Task handle. */
-}  
-
-void reg_ID()
-{
-    strcpy(buf,"Register|");
-    strcat(buf,client_ID);
-    wifiClient.write(buf, strlen(buf));
-    wifiClient.flush();
+void setup() {
+  
+ Serial.begin(9600);
+ pinMode(ledpin1,OUTPUT);
+ pinMode(ledpin2,OUTPUT);
 }
 
-void send_mes(char ID[],char mes[])
-{
-    sprintf(buf,"%s|%s",ID,mes);
-    wifiClient.write(buf, strlen(buf));
-    wifiClient.flush();
-}
+void loop() {
 
-void askPos( void * parameter )
-{
-    while(1)
-    {
-      if ((messageLen = wifiClient.available()) > 0) {
-        int i = 0;
-        do
-        {
-            buf[i++] = wifiClient.read();
-        }while(i<32 && buf[i-1]!='\r');
-        
-        buf[i-1] = '\0';
-        recv_ID = strtok(buf,"|\0");
-        Serial.print(recv_ID);
-        Serial.print(":");
-        recv_buf = strtok(NULL,"|\0");
-        Serial.println(recv_buf);
-        send_mes("Position","");
-      }
-      delay(100);
-    }
-    
-    Serial.println("Ending task 1");
-    vTaskDelete( NULL );
- 
-}
+ analogWrite(ledpin1,HIGH);
+ digitalWrite(ledpin2,LOW);
+ delay(500);
+ digitalWrite(ledpin1,LOW);
+ digitalWrite(ledpin2,HIGH);
+ delay(500);
 
-void loop()
-{   
-    
-    
-    delay(100);
-} 
+}
